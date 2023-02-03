6395b7d5fcba30d15c6287b24de984f645d82e3b
ocramer7@gmail.com <owen.cramer@cecstudents.org>
2023-02-03 14:06:57 -0700

    Initial commit

---
+++ changeMachine.cpp
@@ -0,0 +1,14 @@
+/* Owen Cramer
+ * FRCC CSC2034
+ * February 5, 2023
+ * Write a program named changeMachine that, given a number of pennies, prints the minimum number
+ * of coin denominations in dollars, half-dollar, quarters, dimes, nickels, and pennies. Use the
+ * conditional expression (?:) to print only nonzero numbers of each coin.
+ */
+
+#include <iostream>
+
+int main() {
+    std::cout << "Hello, World!" << std::endl;
+    return 0;
+}
