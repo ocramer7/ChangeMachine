bc30f143b086a882fa075b146d5e12423db8afab
ocramer7@gmail.com <owen.cramer@cecstudents.org>
2023-02-04 12:53:01 -0700

    Finished assignment

--- changeMachine.cpp
+++ changeMachine.cpp
@@ -7,8 +7,38 @@
  */
 
 #include <iostream>
+using namespace std;
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    int dollar = 0, hfDollar = 0, quarter = 0, dime = 0, nickel = 0, penny = 0;
+    int input;
+
+    cout << "Enter the amount of pennies you have: ";
+    cin >> input;
+
+    dollar = input >= 100 ? (input / 100) : dollar;
+    input = input - (dollar * 100);
+
+    hfDollar = input >= 50 ? input / 50 : hfDollar;
+    input = input - (hfDollar * 50);
+
+    quarter = input >= 25 ? input / 25 : quarter;
+    input = input - (quarter * 25);
+
+    dime = input >= 10 ? input / 10 : dime;
+    input = input - (dime * 10);
+
+    nickel = input >= 5 ? input / 5 : nickel;
+    input = input - (nickel * 5);
+
+    penny = input;
+
+    cout << "Dollar(s): " << dollar << "\n";
+    cout << "Half Dollar: " << hfDollar << "\n";
+    cout << "Quarter(s): " << quarter << "\n";
+    cout << "Dime(s): " << dime << "\n";
+    cout << "Nickel(s): " << nickel << "\n";
+    cout << "Pennies: " << penny << "\n";
+
     return 0;
 }
